3f753314d3b12bb88b2dc2e40a2841008c538a30
Julio Paiva <julio.paiva@anlix.io>
2024-09-30 11:34:46 -0300

    Use filepath instead of json string

--- bin/cmd_helper.dart
+++ bin/cmd_helper.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 import 'package:cmd_helper/cmd_helper.dart';
 
 // Main function parses the arguments and runs the command.
-// The input must be a JSON string with the following structure:
+// The input must be a JSON file with the following structure:
 // {
 //   "command": "command",
 //   "args": ["arg1", "arg2"],
@@ -21,12 +21,13 @@ void main(List<String> args) async {
   List<Map<String, dynamic>> jsonList = [];
 
   try {
-    Map<String, dynamic> json = jsonDecode(args.join(' '));
+    String filepath = await File(args[0]).readAsString();
+    Map<String, dynamic> json = jsonDecode(filepath);
     if (json.containsKey('list')) {
       jsonList = List<Map<String, dynamic>>.from(json['list']);
     } else if (!json.containsKey('command') || !json.containsKey('args')) {
       print('Invalid JSON: missing command or args');
-      print('Usage: dart cmd_helper.dart <json>');
+      print('Usage: dart cmd_helper.dart <json file>');
       exit(1);
     } else {
       jsonList.add(json);
